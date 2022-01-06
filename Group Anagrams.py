9c81a43efb8e5f014790dfed544862854a523f2c
haksookim <gkrtn1024@gmail.com>
2022-01-05 19:09:22 -0500

    Add Group Anagrams

---
+++ Group Anagrams.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/group-anagrams/
+# Hak Soo Kim
+# 1/5/2022
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ans = collections.defaultdict(list)
+        for word in strs:
+            sortedWord = tuple(sorted(word))
+            ans[sortedWord].append(word)
+        return ans.values()
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+## better way
+# hmap = collections.defaultdict(list)
+# for st in strs:
+#     array = [0] * 26
+#     for l in st:
+#         array[ord(l) - ord('a')] += 1
+#     hmap[tuple(array)].append(s )
+# return hmap.values()
+
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+#
+# Example 1:
+#
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+# Example 2:
+#
+# Input: strs = [""]
+# Output: [[""]]
+# Example 3:
+#
+# Input: strs = ["a"]
+# Output: [["a"]]
+# Constraints:
+#
+# 1 <= strs.length <= 104
+# 0 <= strs[i].length <= 100
+# strs[i] consists of lowercase English letters.
