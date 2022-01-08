abf4816eb95290b45bdc1f700339ed1d52ec63be
haksookim <gkrtn1024@gmail.com>
2022-01-08 10:44:24 -0500

    Add faster algorithm with Expand Around Center

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -3,20 +3,30 @@
 # 1/3/2022
 
 class Solution(object):
+    maxLen = 0
+    left = 0
     def longestPalindrome(self, s):
-        arr = [[False]*len(s) for i in range(len(s))]
-        startIndex = 0
+        global maxLen
         maxLen = 0
-        for i in reversed(range(len(s))):
-            j = i
-            while(j < len(s)):
-                if ((s[i] == s[j] and j - i < 3) or (s[i] == s[j] and arr[i + 1][j - 1])):
-                    arr[i][j] = True
-                    if (j - i + 1 > maxLen):
-                        maxLen = j - i + 1
-                        startIndex = i
-                j += 1
-        return s[startIndex: startIndex + maxLen]
+        for i in range(len(s)):
+            self.expand(s, i, i)
+            self.expand(s, i, i + 1)
+        return s[left:left + maxLen]
+        """
+        :type s: str
+        :rtype: str
+        """
+
+    def expand(self, s, l, r):
+        while (l >= 0 and r < len(s) and s[l] == s[r]):
+            l -= 1
+            r += 1
+
+        if (maxLen < r - l - 1):
+            global left
+            global maxLen
+            left = l + 1
+            maxLen = r - l - 1
 
 # Given a string s, return the longest palindromic substring in s.
 #
@@ -34,3 +44,20 @@ class Solution(object):
 #
 # 1 <= s.length <= 1000
 # s consist of only digits and English letters.
+
+## dynamic programming algorithm
+class Solution(object):
+    def longestPalindrome(self, s):
+        arr = [[False]*len(s) for i in range(len(s))]
+        startIndex = 0
+        maxLen = 0
+        for i in reversed(range(len(s))):
+            j = i
+            while(j < len(s)):
+                if ((s[i] == s[j] and j - i < 3) or (s[i] == s[j] and arr[i + 1][j - 1])):
+                    arr[i][j] = True
+                    if (j - i + 1 > maxLen):
+                        maxLen = j - i + 1
+                        startIndex = i
+                j += 1
+        return s[startIndex: startIndex + maxLen]
