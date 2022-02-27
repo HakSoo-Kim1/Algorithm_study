1bcdf6183948816d30673d1776a0e0844779018e
haksookim <gkrtn1024@gmail.com>
2022-02-27 16:55:37 -0500

    Add Two Sum

---
+++ Two Sum.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/merge-two-binary-trees/
+# Hak Soo Kim
+# 2/27/2022
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        dic = {}
+        for i, v in enumerate(nums):
+            remainder = target - v
+            if (remainder in dic):
+                return ([dic[remainder], i])
+            dic[v] = i
+        return None
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+#
+# Example 1:
+#
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+#
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+#
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+#
+# Constraints:
+#
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
